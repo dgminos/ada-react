f19f98dcbb746ee73ec529af366a2dad8156f28b
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-01-19 12:05:39 -0300

    muevo carpeta de clase 92 a carpeta de react-js y creo carpeta components para guardar ahí componente Layout y Main

---
+++ clase92/src/components/Layout/Main/components/News/Article/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const Article = () => {
+    return (
+        article = {
+            title: 'articulo 1',
+            date: 'new Date()',
+            author: 'Gabi',
+            text: 'David Keith Lynch, más conocido como David Lynch, es un director de cine, actor, productor de música electrónica y guionista estadounidense. Su actividad artística se extiende asimismo al terreno de la pintura, la música, la publicidad, la fotografía, e incluso el diseño de mobiliario.',
+            comments: '5 comments'
+
+        })
+}
+export default Article
