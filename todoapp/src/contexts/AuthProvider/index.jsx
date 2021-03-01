14d1defdf6ebdb13f70a653a4e43e9fe867cc0c8
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-03-01 15:01:23 -0300

    creo primer componente de context: Authprovider, para autenticar logueo de usuarios

---
+++ todoapp/src/contexts/AuthProvider/index.jsx
@@ -0,0 +1,16 @@
+// import React, { createContext, useState } from 'react'
+
+// const AuthContext = createContext()
+
+// const AuthProvider = ({ children }) => {
+//     const [user, setUser] = useState({});
+//     const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//     return (
+//         <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// }
+
+// export { AuthProvider, AuthContext }
