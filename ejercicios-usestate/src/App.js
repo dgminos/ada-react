3a79617b23019260457ac29bee9a3218acb5f786
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-03-14 13:22:49 -0300

    creo componente Collapse

--- ejercicios-usestate/src/App.js
+++ ejercicios-usestate/src/App.js
@@ -1,12 +1,22 @@
 import { P } from './components'
 import { Collapse } from './components'
 import './App.css';
+import { ChevronUp } from 'react-bootstrap-icons'
+import { ChevronDown } from 'react-bootstrap-icons'
 
 const App = () => {
+  const apertura = <ChevronUp />
+  const cierre = <ChevronDown />
   return (
     <>
       <P />
-      <Collapse />
+      <p>Qué debo hacer para utilizar estado en React?
+         </p>
+      <Collapse label='+'>
+        <p>
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </p>
+      </Collapse>
     </>
   )
 }
