554980c90598a4ce019dd7c5ae87e554f989e758
Adrián <adrian@manjaro.fibertel.com.ar>
2021-03-17 21:11:50 -0300

    Rutas absolutas

--- todoapptypescript/src/App.tsx
+++ todoapptypescript/src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import { AuthProvider } from './contexts';
-import { AuthRoute } from './screens';
-
+import React from "react";
+import { AuthProvider } from "@contexts";
+import { AuthRoute } from "@screens";
 
 const App = () => {
   return (
-
     <AuthProvider>
       <AuthRoute />
     </AuthProvider>
-
-  )
-}
+  );
+};
 
 export default App;
