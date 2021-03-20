6be8f64dcff4661fb8a2d9cc1cd5be9b6c471036
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-03-20 10:20:53 -0300

    tipado de objetcToArray.ts

--- todoapptypescript/src/helpers/objectToArray.ts
+++ todoapptypescript/src/helpers/objectToArray.ts
@@ -1,8 +1,21 @@
-export const objectToArray = (obj) => {
+// export const objectToArray = (obj) => {
+//     let array = [];
+
+//     for (const prop in obj) {
+//         array.push({ ...obj[prop], id: prop });
+//     }
+
+//     return array;
+// };
+
+export const objectToArray = <T>(obj: T): {
+    0: T[keyof T];
+    id: keyof T;
+}[] => {
     let array = [];
 
     for (const prop in obj) {
-        array.push({ ...obj[prop], id: prop });
+        array.push({ 0: obj[prop], id: prop });
     }
 
     return array;
